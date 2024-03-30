84ea879370741f6f8a1c59387b975bbee879a404
piotr <piotr@infordb.com>
2024-03-30 14:45:08 +0100

    - Electron => ESP32 command communication

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 const { usb } = require('usb');
-const path = require('node:path')
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+const { isJson } = require('./electron/utility');
+
+const node_path = require('node:path')
 
 let window;
 
@@ -10,7 +14,7 @@ function createWindow(){
     height: 900,
     backgroundColor: '#292C35',
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: node_path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       enableRemoteModule: false,
     }
@@ -23,9 +27,28 @@ function createWindow(){
 
 app.on('ready', createWindow);
 
-usb.on('attach', device => {
-  window.webContents.send('display-controller-connected', device);
-})
-usb.on('detach', device => {
-  window.webContents.send('display-controller-disconnected', device);
-})
+usb.on('attach', pingPorts);
+usb.on('detach', pingPorts);
+
+async function pingPorts(){
+  const ports = await SerialPort.list()
+
+  for(const port of ports){
+    const serialport = new SerialPort({ baudRate: 115200, path: port.path });
+    const parser = new ReadlineParser();
+    parser.on('data', data => { verifyPort(data, port) });
+    serialport.pipe(parser);
+    serialport.write(JSON.stringify({ command: 'INIT' }));
+  }
+
+  return null;
+}
+function selectPort(port){
+  window.webContents.send('display-controller-connected', port);
+}
+function verifyPort(data, port){
+  data = data.trim();
+  if(data === "OK"){
+    selectPort(port)
+  }
+}
